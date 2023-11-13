ac6336c62f7faf39f0fcb87ee01cd7c212284a03
lucasborba111 <lucasborgesborba@gmail.com>
2023-11-13 20:04:54 -0300

    formulario input completo

--- src/components/forms/FormPartidaChegada.jsx
+++ src/components/forms/FormPartidaChegada.jsx
@@ -6,49 +6,63 @@ import { useTheme } from "@mui/material/styles";
 
 export default function FormPartidaChegada({ calcularPartida, loading }) {
   const outerTheme = useTheme();
+
   return (
     <form className="formulario" onSubmit={calcularPartida}>
-      <h3>Calcuar rota de entrega</h3>
-      <ThemeProvider theme={customTheme(outerTheme)}>
-        <FormControl required>
-          <TextField
-            label="Latitude partida"
-            id="pLat"
-            type="number"
-            inputProps={{ step: "any" }}
-          />
-        </FormControl>
+      <h3>Calcular rota de entrega</h3>
+      <div id="form-content">
+        <ThemeProvider theme={customTheme(outerTheme)}>
+          <FormControl required>
+            <TextField
+              label="Latitude partida"
+              id="pLat"
+              type="number"
+              inputProps={{ step: "any" }}
+            />
+          </FormControl>
 
-        <FormControl required>
-          <TextField
-            label="Longitude partida"
-            id="pLng"
-            type="number"
-            inputProps={{ step: "any" }}
-          />
-        </FormControl>
+          <FormControl required>
+            <TextField
+              label="Longitude partida"
+              id="pLng"
+              type="number"
+              inputProps={{ step: "any" }}
+            />
+          </FormControl>
+          <FormControl required>
+            <TextField
+              label="Latitude chegada"
+              id="pLat"
+              type="number"
+              inputProps={{ step: "any" }}
+            />
+          </FormControl>
 
-        <FormControl required error={false}>
-          <TextField label="Origem" id="origem" type="text" />
-        </FormControl>
-
-        <FormControl required error={false}>
-          <TextField label="Destino" id="destino" type="text" />
-        </FormControl>
-
-        <FormControl required error={false}>
-          <TextField label="Carga" id="carga" type="text" />
-        </FormControl>
-        <LoadingButton
-          type="submit"
-          loading={loading}
-          loadingIndicator="Carregando..."
-          variant="contained"
-          fullWidth={true}
-        >
-          Calcular
-        </LoadingButton>
-      </ThemeProvider>
+          <FormControl required>
+            <TextField
+              label="Longitude chegada"
+              id="pLng"
+              type="number"
+              inputProps={{ step: "any" }}
+            />
+          </FormControl>
+          <FormControl required error={false}>
+            <TextField label="Carga" id="carga" type="text" />
+          </FormControl>
+          <FormControl required error={false}>
+            <TextField label="Peso" id="pedo" type="text" />
+          </FormControl>
+        </ThemeProvider>
+      </div>
+      <LoadingButton
+        type="submit"
+        loading={loading}
+        loadingIndicator="Carregando..."
+        variant="contained"
+        sx={{ position: "relative", marginTop: "auto", width: "80%" }}
+      >
+        Calcular
+      </LoadingButton>
     </form>
   );
 }
